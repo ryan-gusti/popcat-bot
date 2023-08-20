39bc742824de2a889c96264a31cd6ebb2bec8a6a
ryan-gusti <nugrahagusti29@gmail.com>
2023-08-20 23:01:57 +0700

    add get data ID

--- popcat.py
+++ popcat.py
@@ -1,11 +1,12 @@
 import requests
 import time
 
-URL = "https://stats.popcat.click/pop?"
+URL_POP = "https://stats.popcat.click/pop?"
+URL_DATA = "https://leaderboard.popcat.click"
 
-pop_count = int(input("Amount pops (1-800) = "))
-token = input("Token = ")
-t = int(input("Delay = "))
+pop_count = int(input("😾 Amount pops (1-800) = "))
+token = input("🔒 Token = ")
+t = int(input("⌛ Delay = "))
 
 data = {
     'pop_count' : pop_count,
@@ -28,6 +29,11 @@ def result(code):
 
 while True:
     timer(t)
-    print('⚡ Execute')
-    r = requests.post(url=URL, data=data)
-    result(r.status_code)
+    print('🦾 Execute')
+    post_data = requests.post(url=URL_POP, data=data)
+    result(post_data.status_code)
+    print('🦾 Getting Data')
+    get_data = requests.get(url=URL_DATA)
+    total = get_data.json()
+    indonesia = total['ID']
+    print("🚨 {:,}".format(indonesia))
